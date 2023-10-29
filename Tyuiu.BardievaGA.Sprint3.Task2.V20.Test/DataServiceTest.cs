65dcc98aef6300bc061aff6d13b39a68fbece8ed
Rev090LT <mamontosaimon900@gmail.com>
2023-10-29 20:42:22 +0500

    Task2

---
+++ Tyuiu.BardievaGA.Sprint3.Task2.V20.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BardievaGA.Sprint3.Task2.V20.Lib;
+
+namespace Tyuiu.BardievaGA.Sprint3.Task2.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int value = 1;
+            int startValue = 1, stopValue = 6;
+            double res = dataService.GetMultiplySeries(value, startValue, stopValue), wait = 914700.940;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BardievaGA.Sprint3.Task2.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BardievaGA.Sprint3.Task2.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BardievaGA.Sprint3.Task2.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("acf70266-7dd4-4aa7-ad7c-08da7d6d6ebd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
